7bf18f0cfe8efb50c51fa03707f85c61fe643ede
Nick Markwell <nick@duckinator.net>
2010-08-11 02:02:28 -0400

    Split macros out into macros.asm

--- asmirc.asm
+++ asmirc.asm
@@ -1,43 +1,4 @@
-%macro string 1
-  %strlen count %1
-  dd count
-  db %1
-%endmacro
-
-%macro preserve_start 0
-  push edx
-  push ecx
-  push ebx
-  push eax
-%endmacro
-
-%macro preserve_end 0
-  pop eax
-  pop ebx
-  pop ecx
-  pop edx
-%endmacro
-
-%macro print 1
-  preserve_start
-
-  mov eax, %1
-  call _print
-
-  preserve_end
-%endmacro
-
-%macro println 1
-  print %1
-
-  preserve_start
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, newline
-  mov dword edx, 1
-  int 0x80
-  preserve_end
-%endmacro
+%include 'macros.asm'
 
 ; ssize_t send(int socket, const void *buffer, size_t length, int flags);
 ; ssize_t recv(int socket, void *buffer, size_t length, int flags);
---
+++ macros.asm
@@ -0,0 +1,42 @@
+; Macros for asmirc
+
+%macro string 1
+  %strlen count %1
+  dd count
+  db %1
+%endmacro
+
+%macro preserve_start 0
+  push edx
+  push ecx
+  push ebx
+  push eax
+%endmacro
+
+%macro preserve_end 0
+  pop eax
+  pop ebx
+  pop ecx
+  pop edx
+%endmacro
+
+%macro print 1
+  preserve_start
+
+  mov eax, %1
+  call _print
+
+  preserve_end
+%endmacro
+
+%macro println 1
+  print %1
+
+  preserve_start
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, newline
+  mov dword edx, 1
+  int 0x80
+  preserve_end
+%endmacro
